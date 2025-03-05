a5ac33d9f560a84e651a8843b8ed2d9d94dd5475
lama20041 <Lama20071@outlook.sa>
2025-03-05 03:41:03 +0300

    Update mips-1.asm

--- mips-1.asm
+++ mips-1.asm
@@ -1,44 +1,47 @@
 .data
-string1: .asciiz "Enter a number:"
-string2: .asciiz "Enter the secon number:"
-string3: .asciiz "Sum:"
-endLine: .asciiz "\n"
+x: .asciiz "Enter a valuse of x "
+y: .asciiz "Enter a valuse of y "
+
+res: .asciiz "Reslt is = "
+
+
 
 .text
-main:
+##print enterh the x
+li $v0,4
+la $a0,x
+syscall
+##read enterh the x
 
-	li $v0 , 4				#print string1 
-	la $a0 , string1  
-	syscall
+li $v0,5
+syscall
+move $s1,$v0
 
-	li $v0 , 5	 			#read integer
-	syscall
-	
-	move $t0,$v0            #Girdiğimiz integer değeri temporary(geçici) değere akttardık.
-	
-	li $v0 , 4
-	la $a0 , endLine        #Boşluk vermek için. 
-	syscall
-	
-	li $v0 , 4
-	la $a0 , string2        
-	syscall
-	
-	li $v0 , 5
-	syscall
-	
-	move $t1,$v0             
-	
-	li $v0 , 4
-	la $a0 , string3
-	syscall
-	
-	add $t2,$t1,$t0			#İki tane yazdığımız integer değerleri toplayıp $t2 temporary değere aktardık.s
-	li $v0, 1			    #print integer
-	move $a0, $t2			#Temporary değeri a0 a aktardık.
-	syscall
-	
-		 
-	li $v0, 10              #exit
-	syscall
-	
+##print enterh the y
+
+li $v0,4
+la $a0,y
+syscall
+##read enterh the y
+
+li $v0,5
+syscall
+move $s2,$v0
+
+
+add $t1,$s1,$s2
+addi $t2,$s1,15
+addi $t3,$s2,10
+sub $t4,$t1,$t2
+add $s0,$t4,$t3
+
+li $v0,4
+la $a0,res
+syscall
+
+li $v0,1
+move $a0,$s0
+syscall
+
+
+
